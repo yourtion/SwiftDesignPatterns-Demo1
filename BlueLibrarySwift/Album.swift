0d10383198e09ae1ee9c85136b90d0b222869ea1
YourtionGuo <yourtion@gmail.com>
2015-09-17 14:31:56 +0800

    Finish GettingStarted

---
+++ BlueLibrarySwift/Album.swift
@@ -0,0 +1,35 @@
+//
+//  Album.swift
+//  BlueLibrarySwift
+//
+//  Created by YourtionGuo on 9/17/15.
+//  Copyright © 2015 Raywenderlich. All rights reserved.
+//
+
+import UIKit
+
+class Album: NSObject {
+    var title : String!
+    var artist : String!
+    var genre : String!
+    var coverUrl : String!
+    var year : String!
+    
+    init(title: String, artist: String, genre: String, coverUrl: String, year: String) {
+        super.init()
+        self.title = title
+        self.artist = artist
+        self.genre = genre
+        self.coverUrl = coverUrl
+        self.year = year
+    }
+    
+    override var description: String {
+        return "title: \(title)" +
+            "artist: \(artist)" +
+            "genre: \(genre)" +
+            "coverUrl: \(coverUrl)" +
+            "year: \(year)"
+    }
+    
+}
---
+++ BlueLibrarySwift/AlbumView.swift
@@ -0,0 +1,39 @@
+//
+//  AlbumView.swift
+//  BlueLibrarySwift
+//
+//  Created by YourtionGuo on 9/17/15.
+//  Copyright © 2015 Raywenderlich. All rights reserved.
+//
+
+import UIKit
+
+class AlbumView: UIView {
+    
+    private var coverImage: UIImageView!
+    private var indicator: UIActivityIndicatorView!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+    
+    init(frame: CGRect, albumCover: String) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.blackColor()
+        coverImage = UIImageView(frame: CGRectMake(5, 5, frame.size.width - 10, frame.size.height - 10))
+        addSubview(coverImage)
+        indicator = UIActivityIndicatorView()
+        indicator.center = center
+        indicator.activityIndicatorViewStyle = .WhiteLarge
+        indicator.startAnimating()
+        addSubview(indicator)
+    }
+    
+    func highlightAlbum(didHighlightView didHighlightView: Bool) {
+        if didHighlightView == true {
+            backgroundColor = UIColor.whiteColor()
+        } else {
+            backgroundColor = UIColor.blackColor()
+        }
+    }
+}
