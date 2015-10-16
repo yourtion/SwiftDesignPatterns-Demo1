7e6b61c3d73149aefc8999843a16b2397f9d5f90
YourtionGuo <yourtion@gmail.com>
2015-10-16 11:23:05 +0800

    update `Use-Facade`

--- BlueLibrarySwift/LibraryAPI.swift
+++ BlueLibrarySwift/LibraryAPI.swift
@@ -10,13 +10,41 @@ import UIKit
 
 class LibraryAPI: NSObject {
     class var sharedInstance: LibraryAPI {
-        //2
         struct Singleton {
-            //3
             static let instance = LibraryAPI()
         }
-        //4
         return Singleton.instance
     }
+    
+    private let persistencyManager: PersistencyManager
+    private let httpClient: HTTPClient
+    private let isOnline: Bool
+    
+    override init() {
+        persistencyManager = PersistencyManager()
+        httpClient = HTTPClient()
+        isOnline = false
+        
+        super.init()
+    }
+    
+    func getAlbums() -> [Album] {
+        return persistencyManager.getAlbums()
+    }
+    
+    func addAlbum(album: Album, index: Int) {
+        persistencyManager.addAlbum(album, index: index)
+        if isOnline {
+            httpClient.postRequest("/api/addAlbum", body: album.description)
+        }
+    }
+    
+    func deleteAlbum(index: Int) {
+        persistencyManager.deleteAlbumAtIndex(index)
+        if isOnline {
+            httpClient.postRequest("/api/deleteAlbum", body: "\(index)")
+        }
+    }
+
 }
 
